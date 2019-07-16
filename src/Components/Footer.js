59eae55d5f5af3e23f6257bcb968f7cb7e1269cc
Sébastien Jolly <seb_879@hotmail.fr>
2019-07-16 15:48:03 +0200

    footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+
+const Footer = () => (
+    <div className="footer">
+        <h1>Contact Us</h1>
+        <hr/>
+        <span>Siège :</span>
+        <p>2 rue Denis Papin</p>
+        <p>Reims</p>
+        <p>0326958471</p>
+    </div>
+);
+
+export default Footer;
