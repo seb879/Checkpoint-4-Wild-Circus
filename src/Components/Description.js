5021b8d5ef4cff2c1e195d1801474dc370d503c5
Sébastien Jolly <seb_879@hotmail.fr>
2019-07-16 13:39:42 +0200

    description

---
+++ src/Components/Description.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../App.css';
+
+const Description = () => (
+    <div className='description'>
+        <h1>Circus description</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum iaculis justo, vitae aliquet sapien ullamcorper id. Pellentesque eleifend tortor varius velit imperdiet eleifend. Ut est tortor, cursus ac aliquam non, placerat ultricies massa. Maecenas semper libero non urna maximus iaculis. Nulla facilisi. Etiam non nisi vel tellus gravida porta. Cras vitae risus magna. Phasellus mollis a velit ac malesuada. Morbi eros erat, varius mattis pellentesque eu, dapibus et risus. Fusce sit amet est condimentum, cursus ante ac, hendrerit ex. </p>
+    </div>
+)
+export default Description;
